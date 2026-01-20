37d2fcaf1b3a0b4b9dfd61e0e53a43ec2839badc
seleka <selekamahona1994@gmail.com>
2026-01-20 13:12:59 +0530

    Final Build: Added full-site scraper and data visualization

---
+++ visualize.py
@@ -0,0 +1,11 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("scraped_quotes.csv")
+top_authors = df["Author"].value_counts().head(10)
+plt.figure(figsize=(10, 6))
+top_authors.plot(kind="bar", color="skyblue", edgecolor="black")
+plt.title("Top 10 Most Quoted Authors")
+plt.ylabel("Number of Quotes")
+plt.tight_layout()
+plt.savefig("author_chart.png")
+print("✅ Chart saved as author_chart.png!")
